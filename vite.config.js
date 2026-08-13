aa66092ce15ae16cf264f94193364112c5111b27
malikreal <farhankhanbaloch91@gmail.com>
2026-08-13 10:24:11 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
-});
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // This splits out all your node_modules into separate chunks
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
+})
